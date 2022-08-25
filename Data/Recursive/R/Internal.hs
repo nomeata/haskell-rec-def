daf2e981a9ada324f55ef3511af774fe6b4399d9
Joachim Breitner <mail@joachim-breitner.de>
2022-08-25 17:16:03 +0200

    No need for DerivingVia

--- Data/Recursive/R/Internal.hs
+++ Data/Recursive/R/Internal.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
