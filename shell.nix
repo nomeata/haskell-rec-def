7fcce55795770156f15900062246afc702f50aa7
Joachim Breitner <mail@joachim-breitner.de>
2022-09-22 15:07:29 +0200

    Drop CI for ghc-8.6 and 8.4
    the space leak test fails, and I am too lazy to investigate

--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@ let
   myGhc = ghc.withPackages(p : with p; [doctest deepseq ghc-heap-view QuickCheck]);
 in stdenv.mkDerivation rec {
   name = "env";
-  buildInputs = [ myGhc ghcid ];
+  buildInputs = [ myGhc ghcid haskell-ci-unstable ];
   shellHook = ''
     export NIX_GHC=${myGhc}/bin/ghc
     export NIX_GHC_LIBDIR=${myGhc}/lib/ghc-${myGhc.version}
