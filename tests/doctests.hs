d10b2e462ed54b460fec134eba622e159885f385
Joachim Breitner <mail@joachim-breitner.de>
2022-09-22 15:41:55 +0200

    Improve doctests

--- tests/doctests.hs
+++ tests/doctests.hs
@@ -1,9 +1,3 @@
 import Test.DocTest
 main :: IO ()
-main = do
-    -- Why do I have to call them separately?
-    doctest ["--fast", "-package=QuickCheck","Data/Recursive/Bool.hs"]
-    doctest ["--fast", "-package=QuickCheck","Data/Recursive/DualBool.hs"]
-    doctest ["--fast", "-package=QuickCheck","Data/Recursive/Set.hs"]
-    doctest ["--fast", "-package=QuickCheck","Data/Recursive/Map.hs"]
-    doctest ["--fast", "-package=QuickCheck","Data/Recursive/Examples.hs"]
+main = doctest ["--fast", "-package=QuickCheck","Data/"]
