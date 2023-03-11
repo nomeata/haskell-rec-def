570d7eb5083913fb20d2ccbcea88d92ef8be1924
Joachim Breitner <mail@joachim-breitner.de>
2023-03-11 10:38:07 +0100

    Flakify dev setup

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/release-22.11;
+  description = "rec-def development environment";
+  outputs = { self, nixpkgs }: {
+    devShell.x86_64-linux =
+     with nixpkgs.legacyPackages.x86_64-linux;
+     let
+       myGhc = ghc.withPackages(p : with p; [doctest deepseq ghc-heap-view QuickCheck]);
+     in stdenv.mkDerivation rec {
+       name = "env";
+       buildInputs = [ myGhc ghcid haskell-ci ];
+       shellHook = ''
+         export NIX_GHC=${myGhc}/bin/ghc
+         export NIX_GHC_LIBDIR=${myGhc}/lib/ghc-${myGhc.version}
+       '';
+     };
+  };
+}
