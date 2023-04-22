e6ed01436781b895687ab67d52e32368452f7b8c
Joachim Breitner <mail@joachim-breitner.de>
2023-04-22 13:40:53 +0200

    nix: Use mkShell

--- flake.nix
+++ flake.nix
@@ -6,9 +6,9 @@
      with nixpkgs.legacyPackages.x86_64-linux;
      let
        myGhc = ghc.withPackages(p : with p; [doctest deepseq ghc-heap-view QuickCheck]);
-     in stdenv.mkDerivation rec {
+     in mkShell rec {
        name = "env";
-       buildInputs = [ myGhc ghcid haskell-ci ];
+       packages = [ myGhc ghcid haskell-ci ];
        shellHook = ''
          export NIX_GHC=${myGhc}/bin/ghc
          export NIX_GHC_LIBDIR=${myGhc}/lib/ghc-${myGhc.version}
